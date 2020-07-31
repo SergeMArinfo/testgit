d2cb914ecb55299e057deab3264839e1baa84cd0
serge munoz <smtjv@laposte.net>
2020-07-31 10:55:03 +0200

    init page

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <div class="container">
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam aut perferendis sed ipsam inventore ab illum reiciendis, eius cupiditate enim recusandae distinctio nisi cum doloremque dicta quo reprehenderit explicabo optio!
+    </div>
+</body>
+</html>
